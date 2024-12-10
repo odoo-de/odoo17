7b57a154abd77a61f842dc1fcd59df47d600701d
odoo-de <zeeshan15402@gmail.com>
2024-12-10 14:18:05 +0500

    upload module

---
+++ delivery_rep/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Delivery Note New',
+    'version': '0.1',
+    'sequence': 0,
+    'summary': "This module contains all the report customization related to their business nature",
+    'author': 'Zeeshan Khan',
+    'company': '',
+    'website': '',
+    'depends': ['base', 'stock'],
+    'data': [
+        'report/delivery_new.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
